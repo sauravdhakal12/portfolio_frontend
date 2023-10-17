fa03ab55e81a6c660b15f88973a5b03e2bc29b1a
sauravdhakal12 <sauravdhakal121@gmail.com>
2023-10-17 15:50:17 +0545

    #2: Added input field for symbol, quantity and price

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,81 @@
+import { useState } from "react";
+
+const global = [];
+
 const App = () => {
+  const [stockTickerSymbol, setStockTickerSymbol] = useState("");
+  const [stockQuantity, setStockQuantity] = useState("");
+  const [stockPrice, setStockPrice] = useState("");
+
+  // Allow input field to be updated
+  const handleStockTickerSymbol = (event) => {
+    setStockTickerSymbol(event.target.value);
+  }
+
+  const handleStockQuantity = (event) => {
+    setStockQuantity(event.target.value);
+  }
+
+  const handleStockPrice = (event) => {
+    setStockPrice(event.target.value);
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const ts = event.target[0].value;
+    const qt = event.target[1].value;
+    const pr = event.target[2].value;
+
+    if(!ts || !qt || !pr){
+    // todo: Notification of faliure
+      alert("Empty field not allowed");
+      return;
+    }
+
+    const newData = {
+      "tickerSymbol" : ts,
+      "quantity": qt,
+      "price": pr
+    };
+
+    global.push(newData);
+
+    // todo: Notification of success
+    setStockTickerSymbol("");
+    setStockPrice("");
+    setStockQuantity("");
+  }
+
   return (
     <div>
-      <h1>Hello World</h1>
+      <form onSubmit={handleSubmit}>
+
+        {/*Ticker Symbol*/}
+        <input
+          type="text"
+          value={stockTickerSymbol}
+          onChange={handleStockTickerSymbol}
+          placeholder="Ticker Symbol"
+        />
+
+        {/*Stock Quantity*/}
+        <input
+          type="number"
+          value = {stockQuantity}
+          onChange={handleStockQuantity}
+          placeholder="Quantity"
+        />
+
+        {/*Stock Price*/}
+        <input
+          type="text"
+          value = {stockPrice}
+          onChange={handleStockPrice}
+          placeholder="Price"
+        />
+
+        <input type="submit" />
+      </form>
     </div>
   );
 };
